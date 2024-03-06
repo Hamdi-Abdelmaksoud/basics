7eb0d40f8f01cd957a8ee79930a868b23235e631
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-03-06 19:59:12 +0100

    coreconcept component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import {CORE_CONCEPTS} from './data.js';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
@@ -23,7 +33,18 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* les attrributs = props */}
+            <CoreConcept {...CORE_CONCEPTS[0]}/>{/* shortcut if we using the same names*/}
+            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+           
+          </ul>
+        </section>
+
       </main>
     </div>
   );
