2de3f16c981b547c705d7f01a1f6924989f4daa1
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-03-10 23:55:37 +0100

    add  button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
  import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  function handleSelect(selectedButton){
+    //selectedButton => components,jsx,props,state
+ 
+      console.log('hello');
+ 
+  }
   return (
     <div>
       <Header />
@@ -15,10 +22,19 @@ function App() {
             <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
             <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
             <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
-           
           </ul>
         </section>
+<section id='examples'>
+  <h2> </h2>
+  <menu>
+<TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+<TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
+<TabButton onSelect={()=>handleSelect('Props')}>Props</TabButton>
+<TabButton onSelect={()=>handleSelect('State')} >State</TabButton>
+  </menu>
 
+
+</section>
       </main>
     </div>
   );
