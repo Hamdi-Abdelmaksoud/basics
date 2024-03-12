0f83bd79005bc7c20d571fc225f2f11829488fe6
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-03-13 00:11:07 +0100

    rendering content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
- import {CORE_CONCEPTS} from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
+
 
 function App() {
-  function handleSelect(selectedButton){
+  //NB : only on the top level of the function not nested
+  const [selectedTopic, setSelectedTopic] = useState();
+  //selectedTopic is a function
+  let tabContent = <p>Please select a topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+    <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+
+    </div>
+    );
+  }
+  function handleSelect(selectedButton) {
     //selectedButton => components,jsx,props,state
- 
-      console.log('hello');
- 
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+
   }
   return (
     <div>
@@ -18,23 +39,22 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {/* les attrributs = props */}
-            <CoreConcept {...CORE_CONCEPTS[0]}/>{/* shortcut if we using the same names*/}
-            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
-            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
-            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />{/* shortcut if we using the same names*/}
+            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
+            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image} />
+            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
           </ul>
         </section>
-<section id='examples'>
-  <h2> </h2>
-  <menu>
-<TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
-<TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
-<TabButton onSelect={()=>handleSelect('Props')}>Props</TabButton>
-<TabButton onSelect={()=>handleSelect('State')} >State</TabButton>
-  </menu>
-
-
-</section>
+        <section id='examples'>
+          <h2> </h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')} >State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
